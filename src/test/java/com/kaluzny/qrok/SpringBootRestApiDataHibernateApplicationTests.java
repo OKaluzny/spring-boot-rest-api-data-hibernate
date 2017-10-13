e22dd12f37c9fec708a47ba78dfa2a3633b2d90e
Oleg <cryoengineer@gmail.com>
2017-10-13 22:42:21 +0300

    Initial commit

---
+++ src/test/java/com/kaluzny/qrok/SpringBootRestApiDataHibernateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kaluzny.qrok;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRestApiDataHibernateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
