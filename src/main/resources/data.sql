f79d9da394ae8cad84da91fd67801a89f243395f
Oleg <cryoengineer@gmail.com>
2017-10-17 16:07:44 +0300

    #29 Added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,34 @@
+
+INSERT INTO authors (author_first_name, author_last_name, author_sex, author_birth_date)
+VALUES ('FirstName1', 'LastName1', 'MALE', '1979-12-12');
+INSERT INTO authors (author_first_name, author_last_name, author_sex, author_birth_date)
+VALUES ('FirstName2', 'LastName2', 'MALE', '1980-10-05');
+INSERT INTO authors (author_first_name, author_last_name, author_sex, author_birth_date)
+VALUES ('FirstName3', 'LastName3', 'MALE', '1965-02-10');
+INSERT INTO authors (author_first_name, author_last_name, author_sex, author_birth_date)
+VALUES ('FirstName4', 'LastName4', 'MALE', '1984-05-05');
+INSERT INTO authors (author_first_name, author_last_name, author_sex, author_birth_date)
+VALUES ('FirstName5', 'LastName5', 'FEMALE', '1989-10-19');
+
+INSERT INTO books (book_title, book_isbn, book_genre) VALUES ('book1', '123-456-789', 'COMEDY');
+INSERT INTO books (book_title, book_isbn, book_genre) VALUES ('book2', '124-457-790', 'DRAMA');
+INSERT INTO books (book_title, book_isbn, book_genre) VALUES ('book3', '125-458-791', 'SATIRE');
+INSERT INTO books (book_title, book_isbn, book_genre) VALUES ('book4', '126-459-792', 'FANTASY');
+
+INSERT INTO Rewards (reward_title, reward_year) VALUES ('Reward1', 2010);
+INSERT INTO Rewards (reward_title, reward_year) VALUES ('Reward2', 2012);
+INSERT INTO Rewards (reward_title, reward_year) VALUES ('Reward3', 2014);
+
+-- INSERT INTO author_book (author_fk, book_fk) VALUES (1, 1);
+-- INSERT INTO author_book (author_fk, book_fk) VALUES (2, 2);
+-- INSERT INTO author_book (author_fk, book_fk) VALUES (1, 3);
+-- INSERT INTO author_book (author_fk, book_fk) VALUES (4, 4);
+-- INSERT INTO author_book (author_fk, book_fk) VALUES (2, 3);
+-- INSERT INTO author_book (author_fk, book_fk) VALUES (2, 1);
+
+-- INSERT INTO authors_rewards (author_author_id, rewards_reward_id) VALUES (5, 1);
+-- INSERT INTO authors_rewards (author_author_id, rewards_reward_id) VALUES (2, 2);
+-- INSERT INTO authors_rewards (author_author_id, rewards_reward_id) VALUES (1, 3);
+-- INSERT INTO authors_rewards (author_author_id, rewards_reward_id) VALUES (4, 1);
+-- INSERT INTO authors_rewards (author_author_id, rewards_reward_id) VALUES (2, 3);
+-- INSERT INTO authors_rewards (author_author_id, rewards_reward_id) VALUES (1, 2);
